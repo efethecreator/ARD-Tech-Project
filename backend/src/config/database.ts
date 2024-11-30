47a239f51f36f57abe0fab143ba685f4dac24f86
eflantos <eflantos@gmail.com>
2024-11-30 15:19:57 +0300

    Project Directory

---
+++ backend/src/config/database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB as string);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+export default connectDB;

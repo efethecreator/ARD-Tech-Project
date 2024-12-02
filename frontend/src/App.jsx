7d8385be85d4b5b7a52f771b94a7c740b86dfe3c
efethecreator <efe214360@hotmail.com>
2024-12-02 15:21:37 +0300

    düzenleme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import VictimForm from "./pages/VictimForm";
+import ApplicationDetail from "./pages/ApplicationDetail";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/victim" element={<VictimForm />} />
+        <Route path="/application/:id" element={<ApplicationDetail />} /> {/* Yeni rota */}
       </Routes>
     </Router>
   );

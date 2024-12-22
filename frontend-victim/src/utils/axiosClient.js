90543d9e03dd702dca0678812f2bc80e8d616dc0
efethecreator <efe214360@hotmail.com>
2024-12-22 17:50:44 +0300

    .env deleted, axios modified

--- frontend-victim/src/utils/axiosClient.js
+++ frontend-victim/src/utils/axiosClient.js
@@ -1,12 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
+
+const BACK_END_URL = import.meta.env.VITE_BACK_END_URL;
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api', // API'nizin base URL'sini buraya ekleyin
+  baseURL: "http://localhost:5000/api", // API'nizin base URL'sini buraya ekleyin
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     // Diğer gerekli başlıklar eklenebilir
   },
 });
 
 export default axiosClient;
-  

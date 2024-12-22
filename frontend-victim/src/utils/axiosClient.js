99040f7caf1b456e31e533734ce40cf44c3e7246
efethecreator <efe214360@hotmail.com>
2024-12-22 21:46:40 +0300

    backend url edited

--- frontend-victim/src/utils/axiosClient.js
+++ frontend-victim/src/utils/axiosClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BACK_END_URL = import.meta.env.VITE_BACK_END_URL;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api", // API'nizin base URL'sini buraya ekleyin
+  baseURL: BACK_END_URL, // API'nizin base URL'sini buraya ekleyin
   headers: {
     "Content-Type": "application/json",
     // Diğer gerekli başlıklar eklenebilir
